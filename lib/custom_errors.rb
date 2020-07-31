c9558762d12fbd933e3a865dbc443864311650bd
Dazha Brown <dajahbrown@hotmail.com>
2020-07-31 21:21:24 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,17 +7,12 @@ class Person
 
   def get_married(person)
     self.partner = person
-    if person.class != Person
-      raise PartnerError
-    else
-      person.partner = self
-    end
+    person.partner = self
   end
 
-  class PartnerError < StandardError
-  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
+
